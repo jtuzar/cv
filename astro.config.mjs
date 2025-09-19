dd30e6876ffadc82fc211707b423a3caf560898d
jtuzar <jakub@tuzar.me>
2025-09-19 15:10:10 +0200

    remove astro-pdf import

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import pdf from "astro-pdf";
-
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
